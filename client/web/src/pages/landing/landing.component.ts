109e9c867ed8a5e224c69e394dd41541cfb11c03
sh0rtener <domarlaid2@mail.ru>
2026-09-11 20:12:35 +0500

    добавил лендинг

--- client/web/src/pages/landing/landing.component.ts
+++ client/web/src/pages/landing/landing.component.ts
@@ -1,13 +1,23 @@
 import { Component } from "@angular/core";
 import { UnauthHeaderComponent } from "../../widgets/unauthorized-header/ui/unauth-header.component";
+import { PrimaryButtonComponent } from "../../shared/ui/button/button/button.component";
 
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing.component.html',
     styleUrl: './landing.component.scss',
-    imports: [UnauthHeaderComponent]
+    imports: [UnauthHeaderComponent, PrimaryButtonComponent]
 })
 
 export class LandingComponent {
+    rotationLogoX = 0;
+    rotationLogoY = 0;
 
+    onMouseMove(event: MouseEvent) {
+        const x = event.clientX / window.innerWidth;
+        const y = event.clientY / window.innerHeight;
+
+        this.rotationLogoY = (x - 0.5) * 20;
+        this.rotationLogoX = -(y - 0.5) * 20;
+    }
 }
