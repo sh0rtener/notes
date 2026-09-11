3b39a473f320b1604f6458d806986f00798d9fb7
sh0rtener <domarlaid2@mail.ru>
2026-09-11 21:09:21 +0500

    добавил переходы по страницам

--- client/web/src/pages/landing/landing.component.ts
+++ client/web/src/pages/landing/landing.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { UnauthHeaderComponent } from "../../widgets/unauthorized-header/ui/unauth-header.component";
 import { PrimaryButtonComponent } from "../../shared/ui/button/button/button.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing.component.html',
     styleUrl: './landing.component.scss',
-    imports: [UnauthHeaderComponent, PrimaryButtonComponent]
+    imports: [UnauthHeaderComponent, PrimaryButtonComponent, RouterLink]
 })
 
 export class LandingComponent {
--- client/web/src/widgets/unauthorized-header/ui/unauth-header.component.ts
+++ client/web/src/widgets/unauthorized-header/ui/unauth-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { PrimaryButtonComponent } from "../../../shared/ui/button/button/button.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-unauth-header',
     templateUrl: './unauth-header.component.html',
     styleUrl: './unauth-header.component.scss',
-    imports: [PrimaryButtonComponent]
+    imports: [PrimaryButtonComponent, RouterLink]
 })
 
 export class UnauthHeaderComponent {
